56074aae2a3321ab1e4dd72475224a35d60878b0
itscsp <cspoojary7972@gmail.com>
2021-09-01 16:32:57 +0530

    this is very usefull for working with AJAX call and JSON format which has nested array of objects

--- js/script.js
+++ js/script.js
@@ -1,43 +1,91 @@
-//CREATING SOME VARIBLES
-let fact = document.getElementById('fact');
-let factText = document.querySelector('#factText');
+var pageCounter = 1;
+var btn = document.getElementById('btn');
+var animalinfo = document.getElementById('animal-info');
 
-let numberInput = document.querySelector('#numberInput');
+btn.addEventListener("click", function(){
+	var ourRequest = new XMLHttpRequest();
 
-//adding event listner to form tags
-numberInput.addEventListener('input', () =>{
-	let number = numberInput.value;
-	/*
-	//write ajax methods
-	let xhr = new XMLHttpRequest();
-	//GET API ADDRESS
-	xhr.open('GET', 'http://numbersapi.com/'+number);
-	
-	xhr.onload = function() {
-		if(this.status == 200 && number != ''){
-			fact.style.display = 'block';
-			factText.innerText = this.responseText;
+	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+pageCounter+'.json');//HERE WE PASSING DYNAMIC DATA INTO "api"
+	ourRequest.onload = function() {
+		if(ourRequest.status >= 200 && ourRequest.status < 400){
+			var data = JSON.parse(ourRequest.responseText);//CONVERTING INTO "JSON" FORMAT
+			renderHTML(data);
+		} else {
+			console.log("We connected to the server, but it return on error.");
 		}
+	};
+	
+	ourRequest.onerror = function() {
+		console.log("Connection error");
+	};
+
+	ourRequest.send();		
+	pageCounter++;
+	if (pageCounter > 3){
+		btn.classList.add("hide-me");
 	}
-	
-	xhr.send();
-	
-	*/
-	
-	//GETING DATA WITH FETCH method
-	fetch('http://numbersapi.com/'+number)
-		.then(response => response.text())
-		.then(data => {
-			if(number != ''){
-				fact.style.display = 'block';
-				factText.innerText = data;
-			}
-		})
-		.catch(err => console.log(err));
-	
 });
 
+//THIS LINE HAS MANY FOR LOOP BECOUSE WE ARE LOOPING INTO "ARRAY OF OBJECT" WHICH HAS "OBJECT OF ARRAY'S"
+function renderHTML(data){
+	var htmltext = "";
+	
+	for(i = 0; i < data.length; i++){
+		htmltext += "<div class='container'><p>"+data[i].name+" is a "+ data[i].species+" that like to eat"
+		
+		for(ii = 0; ii < data[i].foods.likes.length; ii++){
+			if(ii == 0){
+				htmltext += data[i].foods.likes[ii];
+			}else {
+				htmltext += " and " + data[i].foods.likes[ii];
+			}
+		}
+		
+		htmltext += ' and dislikes ';
+		
+		for(ii = 0; ii < data[i].foods.dislikes.length; ii++){
+			if(ii == 0){
+				htmltext += data[i].foods.dislikes[ii];
+			}else{
+				htmltext += " and " + data[i].foods.dislikes[ii];
+			}
+		}
+		
+		
+		htmltext += '.</p></div>';
+	}
+	
+	animalinfo.insertAdjacentHTML('beforeend', htmltext)
+}
 
+/*
+//JSON FORMAT
 
+[
+  {
+    "name": "Meowsy",
+    "species" : "cat",
+    "foods": {
+      "likes": ["tuna", "catnip"],
+      "dislikes": ["ham", "zucchini"]
+    }
+  },
+  {
+    "name": "Barky",
+    "species" : "dog",
+    "foods": {
+      "likes": ["bones", "carrots"],
+      "dislikes": ["tuna"]
+    }
+  },
+  {
+    "name": "Purrpaws",
+    "species" : "cat",
+    "foods": {
+      "likes": ["mice"],
+      "dislikes": ["cookies"]
+    }
+  }
+]
 
- 
+*/
